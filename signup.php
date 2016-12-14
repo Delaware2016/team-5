7c86ed6cb051ec82f8fded22792420cb001d732c
Force for Good <scott.a.malinowski@jpmchase.com>
2016-12-13 19:31:10 -0500

    update

--- signup.php
+++ signup.php
@@ -45,7 +45,7 @@
 							<span class="icon-bar"></span>
 						  </button>
 						<a class="navbar-brand" href="#">
-							<p class="navbar-crookedwarden"></p>
+							<p class="navbar-crookedwarden">	</p>
 						</a>
 					</div>
 					<div id="navbar" class="collapse navbar-collapse">
